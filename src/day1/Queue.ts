7e1a2bbd8cd58171b58e53f070e9de406d83a07c
gstrand99 <gstrand99@gmail.com>
2023-07-23 15:49:07 -0500

    Linked list queue

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,47 @@
+type QNode<T> = {
+    value: T;
+    next?: QNode<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head: QNode<T> | undefined;
+    private tail: QNode<T> | undefined;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        console.log(this, item)
+        const node = { value: item } as QNode<T>;
+        this.length++;
+        if (!this.tail) {
+            this.tail = this.head = node;
+            return;
+        }
 
-}
+        this.tail.next = node;
+        this.tail = node;
+
+
+    }
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+
+        const head = this.head;
+        this.head = this.head.next;
+
+        head.next = undefined;
+
+        return head.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }

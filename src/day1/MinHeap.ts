10ca5110d5c1256a67a6a7a3dda7a4eef6cbc269
Garrett Strand <gstrand99@gmail.com>
2023-08-15 13:50:38 -0500

    MinHeap

--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -1,15 +1,89 @@
 export default class MinHeap {
-    public length: number;
+  public length: number;
+  private data: number[];
 
-    
+  constructor() {
+    this.data = [];
+    this.length = 0;
+  }
 
-    constructor() {
+  insert(value: number): void {
+    this.data[this.length] = value;
+    this.heapifyUp(this.length);
+    this.length++;
+  }
+
+  delete(): number {
+    if (this.length === 0) {
+      return -1;
+    } 
+
+    const out = this.data[0];
+
+    this.length--;
+    if (this.length == 0) {
+      this.data = [];
+      return out;
     }
 
-    insert(value: number): void {
+    this.data[0] = this.data[this.length];
+    this.heapifyDown(0);
+    return out;
 
-}
-    delete(): number {
+  }
 
-}
+  private heapifyDown(idx: number): void {
+    if (idx >= this.length) {
+      return;
+    }
+
+    const lIdx = this.leftChild(idx);
+    const rIdx = this.rightChild(idx);
+
+    if (idx >= this.length || lIdx >= this.length) {
+      return;
+    }
+    
+    const lV = this.data[lIdx];
+    const rV = this.data[rIdx];
+    const v = this.data[idx];
+
+    if (lV > rV && v > rV) {
+      this.data[idx] = rV;
+      this.data[rIdx] = v;
+      this.heapifyDown(rIdx);
+    } else if (rV > lV && v > lV) {
+      this.data[idx] = lV;
+      this.data[lIdx] = v;
+      this.heapifyDown(lIdx);
+    }
+  }
+
+  private heapifyUp(idx: number): void {
+    if (idx === 0) {
+      return;
+    }
+
+    const p = this.parent(idx);
+    const parenV = this.data[p];
+    const v = this.data[idx];
+
+    if (parenV > v) {
+      this.data[idx] = parenV;
+      this.data[p] = v;
+      this.heapifyUp(p);
+    }
+  }
+
+  private parent(idx: number): number {
+    return Math.floor((idx - 1) / 2);
+  }
+  
+  private leftChild(idx: number): number {
+    return idx * 2 + 1;
+  }
+
+  private rightChild(idx: number): number {
+    return idx * 2 + 2;
+  }
 }

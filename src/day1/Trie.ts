e9ab8142d49adecf792ab30dbdd355069fa22a6e
Garrett Strand <gstrand99@gmail.com>
2023-08-15 14:00:02 -0500

    Trie

--- src/day1/Trie.ts
+++ src/day1/Trie.ts
@@ -1,18 +1,88 @@
+class TrieNode {
+  children: { [key: string]: TrieNode };
+  isEndOfWord: boolean;
+
+  constructor() {
+    this.children = {};
+    this.isEndOfWord = false;
+  }
+}
+
 export default class Trie {
-    
+  private root: TrieNode;
 
-    
+  constructor() {
+    this.root = new TrieNode();
+  }
 
-    constructor() {
+  insert(item: string): void {
+    let currentNode = this.root;
+
+    for (const char of item) {
+      if (!currentNode.children[char]) {
+        currentNode.children[char] = new TrieNode();
+      }
+      currentNode = currentNode.children[char];
     }
 
-    insert(item: string): void {
+    currentNode.isEndOfWord = true;
+  }
 
-}
-    delete(item: string): void {
+  delete(item: string): void {
+    const deleteRecursively = (node: TrieNode, item: string, index: number): boolean => {
+      if (index === item.length) {
+        if (node.isEndOfWord) {
+          node.isEndOfWord = false;
+          return Object.keys(node.children).length === 0;
+        }
+        return false;
+      }
 
-}
-    find(partial: string): string[] {
+      const char = item[index];
+      const nextNode = node.children[char];
 
+      if (!nextNode) {
+        return false;
+      }
+
+      const shouldDeleteChild = deleteRecursively(nextNode, item, index + 1);
+
+      if (shouldDeleteChild) {
+        delete node.children[char];
+        return Object.keys(node.children).length === 0 && !node.isEndOfWord;
+      }
+
+      return false;
+    };
+
+    deleteRecursively(this.root, item, 0);
+  }
+
+  find(partial: string): string[] {
+    let currentNode = this.root;
+
+    for (const char of partial) {
+      if (!currentNode.children[char]) {
+        return [];
+      }
+      currentNode = currentNode.children[char];
+    }
+
+    const results: string[] = [];
+
+    const traverse = (node: TrieNode, prefix: string) => {
+      if (node.isEndOfWord) {
+        results.push(prefix);
+      }
+
+      for (const char in node.children) {
+        traverse(node.children[char], prefix + char);
+      }
+    };
+
+    traverse(currentNode, partial);
+
+    return results;
+  }
 }
-}
+
